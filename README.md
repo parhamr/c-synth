842921e49f8e2a01bccf7e30015814ea9bec97cf
parhamr <reid.parham@gmail.com>
2013-09-18 18:13:24 +0000

    Basic guitar synthesizer.

---
+++ README.md
@@ -0,0 +1,11 @@
+# C Synth
+
+A music synthesizer in C.
+
+# Compiling
+
+gcc -o synth -x c synth.c
+
+# Usage
+
+./synth 220 </dev/urandom | aplay -d 3
---
+++ synth.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// Audio synthesizer
+
+int main (int i, char **a)
+{
+/* Accepts integer and char arguments and then returns a waveform representation of the integer
+   as a decaying sound, much like a plucked electric guitar string.
+   */
+  // math; 16 bit raise to the power of 3??? input char
+  int s=16e3 / atoi(a[1]);
+  //
+  unsigned char v[s];
+  // loads stdin to v as buffer of length s
+  if(read(0,v,s) < 0) {
+    write(2, "FATAL: could not read input.\n\n", 31);
+    return 1;
+  }
+  // infinite loop
+  for(;;){
+    //
+    putchar(v[i%s]=( v[i%s] + v[++i%s] ) / 2);
+  }
+}
+
